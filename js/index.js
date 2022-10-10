3947c6ed21264e0db638539893488003c836ccb7
Nick Griffey <nick.griffey@protonmail.com>
2022-10-10 00:26:05 -0400

    Update cart as items are added/deleted

--- js/index.js
+++ js/index.js
@@ -36,46 +36,78 @@ class StoreItems {
 
 class ShoppingCart {
 	constructor() {
+		this.storeItems = new StoreItems()
+		this.userList = new ShoppingList()
+
 		this.items = {}
 		this.total = 0
 	}
 
 	addToCart(itemName, quantity) {
 		if (itemName in this.items) {
-			this.items[itemName] += quantity
+			this.items[itemName]["quantity"] += quantity
 		} else {
-			this.items[itemName] = quantity
+			let price = this.storeItems.items[itemName]["unitPrice"]
+			this.items[itemName] = {"quantity": quantity, "unitPrice": price}
 		}
 
 		this.updateTotal()
+		this.updateUI()
 	}
 
 	removeFromCart(itemName, quantity) {
 		if (itemName in this.items) {
-			this.items[itemName] -= quantity
+			this.items[itemName]["quantity"] -= quantity
 		}
 
-		if (this.items[itemName] <= 0) {
+		if (this.items[itemName]["quantity"] <= 0) {
 			delete this.items[itemName]
 		}
 
 		this.updateTotal()
+		this.updateUI()
 	}
 
 	updateTotal() {
 		this.total = 0
-		for (item in this.items) {
-			this.total += this.items[item]
+		for (const item in this.items) {
+			let unitPrice = this.items[item]["unitPrice"]
+			let quantity = this.items[item]["quantity"]
+			this.total += unitPrice * quantity
 		}
+	}
+
+	addToCartClick() {
+		let itemName = document.getElementById("item-name").value
+		let quantity = Number(document.getElementById("item-quantity").value)
+
+		this.addToCart(itemName, quantity)
+	}
+
+	removeFromCartClick() {
+		let itemName = document.getElementById("item-name").value
+		let quantity = Number(document.getElementById("item-quantity").value)
+
+		this.removeFromCart(itemName, quantity)
+	}
+
+	updateUI() {
+		let cartList = document.getElementById("cart-list")
+
+		// clear list
+		cartList.innerHTML = ""
+
+		for (const item in this.items) {
+			let li = document.createElement('li')
+			let price = this.items[item]["unitPrice"]
+			let quantity = this.items[item]["quantity"]
+			li.textContent = item + " (" + quantity + " @ $" + price +  ")"
+			cartList.appendChild(li)
+		}
+
+		let cartTotal = document.getElementById("cart-total")
+		cartTotal.innerHTML = "$" + this.total.toFixed(2)
 	}
 }
 
-function addToCartClick(cart) {
-	itemName = document.getElementById("item-name").value
-	quantity = Number(document.getElementById("item-quantity").value)
-
-	cart.addToCart(itemName, quantity)
-}
-
-let storeItems = new StoreItems()
-let userList = new ShoppingList()
+let userCart = new ShoppingCart()
