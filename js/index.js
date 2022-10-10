453f51e49c4ac10b93dcc05457a2189a7c0aed11
Nick Griffey <nick.griffey@protonmail.com>
2022-10-09 23:41:12 -0400

    Consolidate js into a few objects

--- js/index.js
+++ js/index.js
@@ -1,23 +1,81 @@
-function populateListElement(data) {
-	itemList = document.getElementById("item-list")
+class ShoppingList {
+	constructor() {
+		fetch("./js/list.json")
+			.then(response => response.json())
+			.then(jsonObject => {
+				this.items = jsonObject
+				this.populateListElement()
+			})
+	}
 
-	for (const item of data) {
-		li = document.createElement('li')
-		li.textContent = item["itemName"] + " (" + item["unitQuantity"] + ")"
-		itemList.appendChild(li)
+	populateListElement() {
+		let itemList = document.getElementById("item-list")
+
+		// clear list
+		itemList.innerHTML = ""
+
+		for (const item in this.items) {
+			let li = document.createElement('li')
+			let unit = this.items[item]["unit"]
+			let quantity = this.items[item]["unitQuantity"]
+			li.textContent = item + " (" + quantity + " " + unit +  ")"
+			itemList.appendChild(li)
+		}
 	}
 }
 
-function populateList() {
-	fetch("./js/list.json")
-	.then(response => response.json())
-	.then(jsonObject => populateListElement(jsonObject))
+class StoreItems {
+	constructor() {
+		fetch("./js/items.json")
+			.then(response => response.json())
+			.then(jsonObject => {
+				this.items = jsonObject
+			})
+	}
 }
 
-function getItems() {
-	fetch("./js/items.json")
-	.then(response => response.json())
-	.then(jsonObject => console.log(jsonObject))
+class ShoppingCart {
+	constructor() {
+		this.items = {}
+		this.total = 0
+	}
+
+	addToCart(itemName, quantity) {
+		if (itemName in this.items) {
+			this.items[itemName] += quantity
+		} else {
+			this.items[itemName] = quantity
+		}
+
+		this.updateTotal()
+	}
+
+	removeFromCart(itemName, quantity) {
+		if (itemName in this.items) {
+			this.items[itemName] -= quantity
+		}
+
+		if (this.items[itemName] <= 0) {
+			delete this.items[itemName]
+		}
+
+		this.updateTotal()
+	}
+
+	updateTotal() {
+		this.total = 0
+		for (item in this.items) {
+			this.total += this.items[item]
+		}
+	}
 }
 
-populateList()
+function addToCartClick(cart) {
+	itemName = document.getElementById("item-name").value
+	quantity = Number(document.getElementById("item-quantity").value)
+
+	cart.addToCart(itemName, quantity)
+}
+
+let storeItems = new StoreItems()
+let userList = new ShoppingList()
