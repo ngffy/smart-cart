9389f632621a17e55ad542e1f02a85a7e8d10d39
Nick Griffey <nick.griffey@protonmail.com>
2022-10-14 16:34:07 -0400

    Extend pause between simulation actions to 2 seconds

--- js/index.js
+++ js/index.js
@@ -234,7 +234,7 @@ class ShoppingCart {
 				this.payClick()
 			}
 
-			await new Promise(resolve => setTimeout(resolve, 1000))
+			await new Promise(resolve => setTimeout(resolve, 2000))
 		}
 	}
 }
